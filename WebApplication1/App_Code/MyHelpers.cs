30283f23f33d45666c22ed3618d501c9723c5a3b
АтчановА <@DESKTOP-GG3G2B2>
2019-09-20 21:08:04 +0600

    replase html helper for author

--- WebApplication1/App_Code/MyHelpers.cs
+++ WebApplication1/App_Code/MyHelpers.cs
@@ -44,22 +44,25 @@ namespace WebApplication1.App_Code
         public static MvcHtmlString EditAuthors(this HtmlHelper html, AuthorModel item)
         {
             TagBuilder form = new TagBuilder("form");
-            form.MergeAttribute("action", "/author/EditAndCreate/"+item.Id);
+            form.MergeAttribute("action", "/author/EditAndCreate/"+item.Id.ToString());
             form.MergeAttribute("data-ajax", "true");
             form.MergeAttribute("data-ajax-method", "POST");
             form.MergeAttribute("data-ajax-mode", "replase");
             form.MergeAttribute("data-ajax-update", "#authors-content");
             form.MergeAttribute("method", "post");
-
+            var d = item;
             TagBuilder formBlock = new TagBuilder("div");
             formBlock.AddCssClass("form-horizontal");
 
-            TagBuilder id = new TagBuilder("input");
-            id.MergeAttribute("id", "Id");
-            id.MergeAttribute("name", "Id");
-            id.MergeAttribute("type", "hidden");
-            id.MergeAttribute("value", item.Id.ToString());
-            formBlock.InnerHtml += id.ToString();
+            if (item.Id != 0)
+            {
+                TagBuilder id = new TagBuilder("input");
+                id.MergeAttribute("id", "Id");
+                id.MergeAttribute("name", "Id");
+                id.MergeAttribute("type", "hidden");
+                id.MergeAttribute("value", item.Id.ToString());
+                formBlock.InnerHtml += id.ToString();
+            }
 
             TagBuilder formGroup = new TagBuilder("div");
             formGroup.AddCssClass("form-group");
@@ -77,6 +80,7 @@ namespace WebApplication1.App_Code
             firstName.MergeAttribute("id", "FirstName");
             firstName.MergeAttribute("name", "FirstName");
             firstName.MergeAttribute("type", "text");
+            if(item.FirstName!=null)
             firstName.MergeAttribute("value", item.FirstName.ToString());
             blockLabel.InnerHtml = firstName.ToString();
             formGroup.InnerHtml += label.ToString();
@@ -84,54 +88,55 @@ namespace WebApplication1.App_Code
             formBlock.InnerHtml += formGroup.ToString();
 
 
-            formGroup = new TagBuilder("div");
-            formGroup.AddCssClass("form-group");         
-            blockLabel = new TagBuilder("div");
-            blockLabel.AddCssClass("col-md-10");
-            firstName = new TagBuilder("input");
-            firstName.AddCssClass("form-control");
-            firstName.AddCssClass("text-box");
-            firstName.AddCssClass("single-line");
-            firstName.MergeAttribute("id", "LastName");
-            firstName.MergeAttribute("name", "LastName");
-            firstName.MergeAttribute("type", "text");
-            firstName.MergeAttribute("value", item.LastName.ToString());
-            blockLabel.InnerHtml = firstName.ToString();
-            formGroup.InnerHtml += blockLabel.ToString();
-            formBlock.InnerHtml += formGroup.ToString();
-
-
-            formGroup = new TagBuilder("div");
-            formGroup.AddCssClass("form-group");
+            TagBuilder formGroup2 = new TagBuilder("div");
+            formGroup2.AddCssClass("form-group");
             label = new TagBuilder("label");
             label.AddCssClass("control-label");
             label.AddCssClass("col-md-2");
             label.MergeAttribute("for", "LastName");
             label.SetInnerText("LastName");
             blockLabel = new TagBuilder("div");
+            blockLabel.AddCssClass("col-md-10");
+            TagBuilder lastName = new TagBuilder("input");
+            lastName.AddCssClass("form-control");
+            lastName.AddCssClass("text-box");
+            lastName.AddCssClass("single-line");
+            lastName.MergeAttribute("id", "LastName");
+            lastName.MergeAttribute("name", "LastName");
+            lastName.MergeAttribute("type", "text");
+            if (item.LastName != null)
+                lastName.MergeAttribute("value", item.LastName.ToString());
+            blockLabel.InnerHtml = lastName.ToString();
+            formGroup2.InnerHtml += label.ToString();
+            formGroup2.InnerHtml += blockLabel.ToString();
+            formBlock.InnerHtml += formGroup2.ToString();
+
+
+            TagBuilder formGroup3 = new TagBuilder("div");
+            formGroup3.AddCssClass("form-group");
+            blockLabel = new TagBuilder("div");
             blockLabel.AddCssClass("col-md-offset-2");
             blockLabel.AddCssClass("col-md-10");
 
-            firstName = new TagBuilder("button");
-            firstName.AddCssClass("btn");
-            firstName.AddCssClass("btn-success");
-            firstName.MergeAttribute("type", "submit");
-            firstName.MergeAttribute("name", "LastName");
-            firstName.MergeAttribute("role", "button");
-            firstName.SetInnerText("Save");
-            blockLabel.InnerHtml += firstName.ToString();
+            TagBuilder button1 = new TagBuilder("button");
+            button1.AddCssClass("btn");
+            button1.AddCssClass("btn-success");
+            button1.MergeAttribute("type", "submit");
+            button1.MergeAttribute("role", "button");
+            button1.SetInnerText("Save");
+            blockLabel.InnerHtml += button1.ToString();
 
-            firstName = new TagBuilder("button");
-            firstName.AddCssClass("btn");
-            firstName.AddCssClass("btn-secondary");
-            firstName.MergeAttribute("data-dismiss", "modal");
-            firstName.MergeAttribute("data-target", "#exampleModalScrollable");
-            firstName.MergeAttribute("role", "button");
-            firstName.SetInnerText("Close");
-            blockLabel.InnerHtml += firstName.ToString();
+            TagBuilder button2 = new TagBuilder("button");
+            button2.AddCssClass("btn");
+            button2.AddCssClass("btn-secondary");
+            button2.MergeAttribute("data-dismiss", "modal");
+            button2.MergeAttribute("data-target", "#exampleModalScrollable");
+            button2.MergeAttribute("role", "button");
+            button2.SetInnerText("Close");
+            blockLabel.InnerHtml += button2.ToString();
 
-            formGroup.InnerHtml += blockLabel.ToString();
-            formBlock.InnerHtml += formGroup.ToString();
+            formGroup3.InnerHtml += blockLabel.ToString();
+            formBlock.InnerHtml += formGroup3.ToString();
 
 
             form.InnerHtml = formBlock.ToString();
